3cba3f8cbae85fcd39d341436808d9d97157b78d
TOKIO777 <86084557+KARINE001@users.noreply.github.com>
2023-08-19 01:51:23 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,20 @@
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<iframe
+<template>
+  <div>
+    <iframe
     src="https://embed.ipfscdn.io/ipfs/bafybeihazpt6pkm4azgtupdz7hc2j3o4zpjsvtcgfq4t2keozxkss3ud6q/?contract=0xD7Fc6e172753fCf452119a3E949Ac3fecF3129C7&chain=%7B%22name%22%3A%22Mode+Testnet+%22%2C%22chain%22%3A%22ModeTest%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Fmode-testnet.rpc.thirdweb.com%2F15065ae3c21e0bff07eaf80b713a6ef0%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22Ether%22%2C%22symbol%22%3A%22ETH%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22ModeTest%22%2C%22chainId%22%3A919%2C%22testnet%22%3Atrue%2C%22slug%22%3A%22mode-testnet%22%2C%22icon%22%3A%7B%22height%22%3A2160%2C%22width%22%3A2160%2C%22format%22%3A%22png%22%2C%22url%22%3A%22ipfs%3A%2F%2Fbafkreidi5y7afj5z4xrz7uz5rkg2mcsv2p2n4ui4g7q4k4ecdz65i2agou%22%7D%7D&clientId=&primaryColor=purple"
     width="600px"
     height="600px"
     style="max-width:100%;"
     frameborder="0"
 ></iframe>
+  </div>
+  <HelloWorld msg="Mode Opepen" />
+</template>
+
+
 
 <style scoped>
 
