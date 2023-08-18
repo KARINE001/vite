2bdb2b5daf3539755dc97fa30f438a50bd916ee8
TOKIO777 <86084557+KARINE001@users.noreply.github.com>
2023-08-19 01:43:00 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -11,20 +11,9 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Mode Opepen" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
